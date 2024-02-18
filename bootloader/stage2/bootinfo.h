3626b458253733879b8e5a8bfe8c7b5eec03196d
F4LCn <naoufel.jamali@gmail.com>
2024-02-18 13:47:38 +0100

    housekeeping: change the structure of bootinfo to have better alignment

--- bootloader/stage2/bootinfo.h
+++ bootloader/stage2/bootinfo.h
@@ -6,14 +6,16 @@ typedef struct __attribute((packed)) {
   u8 magic[4];
   u32 size;
   u8 bootloader_type;
+  u8 unused0[3];
   u64 fb_ptr;
   u32 fb_width;
   u32 fb_height;
   u32 fb_scanline_bytes;
   u8 fb_pixelformat;
-  u8 unused0[34];
+  u8 unused1[31];
   u64 acpi_ptr;
-  u8 unused1[24];
+  u8 unused2[24];
+  u8 mmap;
 } boot_info;
 
 extern boot_info bootinfo;
