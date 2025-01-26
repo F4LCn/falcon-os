f814164f0fb81c5ded60d5a3262afcb530febe8d
F4LCn <naoufel.jamali@gmail.com>
2025-01-26 12:30:32 +0100

    (feat) early heap allocation

--- kernel/build.zig
+++ kernel/build.zig
@@ -13,9 +13,17 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
         .root_source_file = b.path("src/main.zig"),
+        .sanitize_thread = false,
+        .omit_frame_pointer = true,
         .pic = true,
         .code_model = .kernel,
     });
+
+    switch (optimize) {
+        .Debug => kernel_exe.root_module.strip = false,
+        else => kernel_exe.root_module.strip = true,
+    }
+
     kernel_exe.setLinkerScript(b.path("linker.ld"));
     b.installArtifact(kernel_exe);
 }
--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -3,6 +3,7 @@ const BootInfo = @import("bootinfo.zig").BootInfo;
 const logger = @import("log/logger.zig");
 const cpu = @import("cpu.zig");
 const serial = @import("log/serial.zig");
+const heap = @import("heap.zig");
 
 extern var bootinfo: *const BootInfo;
 
@@ -25,4 +26,30 @@ pub export fn kernelMain() callconv(.c) void {
     logger.init(serial.Port.COM1);
     cpu.init();
     std.log.info("Cpu vendor id: {s}", .{cpu.cpu_info.vendor_str});
+
+    failableMain() catch |e| {
+        std.log.err("Failed with error: {any}", .{e});
+    };
+}
+
+pub fn failableMain() !void {
+    const alloc = heap.allocator();
+    var list = std.ArrayList(u32).init(alloc);
+    try list.append(1);
+    try list.append(10);
+    try list.append(5434);
+
+    for (list.items, 0..) |item, i| {
+        std.log.info("item[{d}] = {d}", .{ i, item });
+    }
+
+    const permAlloc = heap.permanentAllocator();
+    var list2 = std.ArrayList(u32).init(permAlloc);
+    try list2.append(1);
+    try list2.append(10);
+    try list2.append(5434);
+
+    for (list2.items, 0..) |item, i| {
+        std.log.info("item[{d}] = {d}", .{ i, item });
+    }
 }
