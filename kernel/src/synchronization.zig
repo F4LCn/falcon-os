8abb5ef9e524ed8db8cfe1df1393f6193fd96b22
F4LCn <naoufel.jamali@gmail.com>
2025-02-22 11:50:47 +0100

    (feat) physical memory manager (WIP)

---
+++ kernel/src/synchronization.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+const INVALID_CPU_ID = std.math.maxInt(u32);
+pub const SpinLock = struct {
+    const Self = @This();
+    cpu_id: u32,
+    locked: bool,
+    lock_count: u32,
+    saved_int_state: bool,
+
+    pub fn create() Self {
+        return .{
+            .cpu_id = INVALID_CPU_ID,
+            .locked = false,
+            .lock_count = 0,
+            .saved_int_state = false,
+        };
+    }
+
+    pub fn lock(self: Self) void {
+        _ = self;
+    }
+
+    pub fn unlock(self: Self) void {
+        _ = self;
+    }
+};
