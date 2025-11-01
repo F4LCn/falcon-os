f931b60854000fe057fa28b5d5fb68d8efc2f03c
Falcon <naoufel.jamali@gmail.com>
2025-11-01 17:58:12 +0100

    (feat) add cpu counting and masking

--- kernel/src/arch/x86_64/cpu.zig
+++ kernel/src/arch/x86_64/cpu.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const assembly = @import("assembly.zig");
 
+const CpuId = u32;
+
 const max_basic_level = 32;
 const max_extended_level = 32;
 const max_fn04_level = 8;
--- kernel/src/cpu.zig
+++ kernel/src/cpu.zig
@@ -1,7 +1,18 @@
 const std = @import("std");
 const arch = @import("arch");
+const options = @import("options");
 
-pub var cpu_info: *arch.cpu.CpuInfo = undefined;
+pub const cpu_info: *arch.cpu.CpuInfo = undefined;
+pub const max_cpu_count = options.max_cpu;
+pub const possible_cpus_mask: std.bit_set.ArrayBitSet(max_cpu_count) = .initEmpty();
+pub var present_cpus_count: u16 = 1;
+pub var present_cpus_mask: std.bit_set.ArrayBitSet(max_cpu_count) = .initEmpty();
+pub var online_cpus_count: u16 = 1;
+pub var online_cpus_mask: std.bit_set.ArrayBitSet(max_cpu_count) = .initEmpty();
+
+comptime {
+    possible_cpus_mask.setRangeValue(.{ .start = 0, .end = max_cpu_count }, true);
+}
 
 pub fn init() !void {
     cpu_info = try arch.cpu.init();
@@ -9,4 +20,14 @@ pub fn init() !void {
 
 pub fn hasFeature(feature: arch.cpu.Feature) bool {
     return arch.cpu.hasFeature(feature);
+}
+
+pub fn setCpuPresent(cpu_id: arch.cpu.CpuId) void {
+    present_cpus_mask.set(cpu_id);
+    present_cpus_count = present_cpus_mask.count();
+}
+
+pub fn setCpuOnline(cpu_id: arch.cpu.CpuId) void {
+    online_cpus_mask.set(cpu_id);
+    online_cpus_count = online_cpus_mask.count();
 }
